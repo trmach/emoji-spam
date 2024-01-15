ec316220aadf96c1fef1c3101e73c86ff02919a0
trmach <charlessaineghi@gmail.com>
2024-01-14 22:27:09 -0600

    cleaned up fuzzy matching

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,6 @@ use emojis;
 
 fn main() {
     let matcher = SkimMatcherV2::default();
-//  assert_eq!(None, matcher.fuzzy_match("abc", "abx"));
-//  assert!(matcher.fuzzy_match("axbycz", "abc").is_some());
-//  assert!(matcher.fuzzy_match("axbycz", "xyz").is_some());
-
-//  let (score, indices) = matcher.fuzzy_indices("axbycz", "abc").unwrap();
-//  assert_eq!(indices, [0, 2, 4]);
-
     let input: String;
     if env::args().len() == 1 {
         input = r#"According to all known laws of aviation, there is no way 
@@ -25,6 +18,7 @@ because bees don't care what humans think is impossible."#.to_string();
 
     let mut map = HashMap::new();
 
+    // Make a dictionary of emojis.
     for e in emojis::iter().map(|e| e.as_str()).collect::<Vec<_>>() {
         let item = emojis::get(e).unwrap();
         map.insert(item.name(), item);
@@ -33,12 +27,21 @@ because bees don't care what humans think is impossible."#.to_string();
     let mut output: String = Default::default();
     for word in input.split(' ') {
         output += format!("{} ", word).as_str();
-        for k in map.keys() {
-            if matcher.fuzzy_match(k, word).is_some() {
-                println!("{} -> {}", k, map.get(k).unwrap().as_str());
-                output += format!("{} ", map.get(k).expect("Map returned none.").to_string()).as_str();
-                break;
+        let mut options: Vec<(i64, String)> = vec![];
+
+        // Fuzzy matching
+        for (k, v) in &map {
+            let option = matcher.fuzzy_match(k, word);
+            if option.is_some() {
+                options.push((option.unwrap(), v.to_string()));
             }
+        }
+
+        // sort and reverse to get the best matches
+        options.sort_by_key(|k| k.0);
+        options.reverse();
+        if options.len() > 0 {
+            output += format!("{} ", options[0].1).as_str();
         }
     }
     println!("{}", output);
